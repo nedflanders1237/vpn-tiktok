5f5e7798643d6c7f009d0ff18e1e28fe01141f81
nedflanders1237 <benhawkins@tutamail.com>
2026-06-14 01:09:36 +0100

    Update password.js

--- welcome/next/password.js
+++ welcome/next/password.js
@@ -159,7 +159,7 @@ btn.addEventListener("click", () => {
 
   setTimeout(() => {
     window.location.href = "/vpn-tiktok/continue/submit/sms.html";
-  }, 7500);
+  }, 5500);
 });
 
 const errorText = document.getElementById("errorText");
